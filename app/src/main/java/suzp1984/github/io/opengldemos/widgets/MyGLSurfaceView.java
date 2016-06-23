9433c69077cdc3fb5a362d4ce2df6946117a6d3c
suzp1984 <suzp1984@gmail.com>
2016-06-23 15:59:45 +0800

    add continuous rotation rendering

--- app/src/main/java/suzp1984/github/io/opengldemos/widgets/MyGLSurfaceView.java
+++ app/src/main/java/suzp1984/github/io/opengldemos/widgets/MyGLSurfaceView.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
 import android.opengl.Matrix;
+import android.os.SystemClock;
 
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
@@ -24,7 +25,7 @@ public class MyGLSurfaceView extends GLSurfaceView {
         mRender = new MyGLRender();
 
         setRenderer(mRender);
-        setRenderMode(RENDERMODE_WHEN_DIRTY);
+        // setRenderMode(RENDERMODE_WHEN_DIRTY);
     }
 
     public static class MyGLRender implements GLSurfaceView.Renderer {
@@ -35,6 +36,8 @@ public class MyGLSurfaceView extends GLSurfaceView {
         private final float[] mMVPMatrix = new float[16];
         private final float[] mProjectionMatrix = new float[16];
         private final float[] mViewMatrix = new float[16];
+
+        private float[] mRotationMatrix = new float[16];
 
         @Override
         public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
@@ -55,6 +58,8 @@ public class MyGLSurfaceView extends GLSurfaceView {
 
         @Override
         public void onDrawFrame(GL10 gl10) {
+            float[] scratch = new float[16];
+
             GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
 
             // Set the camera position (View Matrix)
@@ -63,7 +68,15 @@ public class MyGLSurfaceView extends GLSurfaceView {
             // Calculate the projection and view transformation
             Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mViewMatrix, 0);
 
-            mTriangle.draw(mMVPMatrix);
+            // create a rotation transformation for the triangle
+            long time = SystemClock.uptimeMillis() % 4000L;
+            float angle = 0.090f * ((int) time);
+
+            Matrix.setRotateM(mRotationMatrix, 0, angle, 0, 0, -1.0f);
+
+            Matrix.multiplyMM(scratch, 0, mMVPMatrix, 0, mRotationMatrix, 0);
+
+            mTriangle.draw(scratch);
         }
     }
 }
